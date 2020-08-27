8058bde5c7742221e845cecdbba1b08207b17c94
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-27 22:25:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese()
+def find_the_cheese(caseiculture)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
