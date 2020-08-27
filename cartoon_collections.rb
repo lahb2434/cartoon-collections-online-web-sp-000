18fcdbbc7511910a4f7e4af146c73c370681cf47
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-27 21:05:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, status| 
     status += 1
     puts "#{status}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
