b686991abbaabea13da6e76a73f3ea94aa34bb23
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-27 21:13:51 +0000

    Automatically backed up by Learn

--- saveme.rb
+++ saveme.rb
@@ -1,3 +1,8 @@
 veggie = %w(apple banana pear peach plum)
 
+def summon_captain_planet(veggie)
+  new_veggie = veggie.map{|x| puts x.capitalize'!' }
+  new_veggie
+end
 
+summon_captain_planet(veggie)
